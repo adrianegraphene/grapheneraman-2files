809c230760cb5d441ec9c58884d88351334b11c2
b252826 <53101996+b252826@users.noreply.github.com>
2019-08-19 14:47:48 -0700

    Updated version
    graph still kind of weird, naming combined files don't work

--- 2RamanFile.py
+++ 2RamanFile.py
@@ -5,16 +5,19 @@
 
 ###This is the code for sorted files (C1 folder, C2 folder...)
 import numpy as np
+from array import *
 #import simplejson
 import pandas as pd
+import array
 import matplotlib.pyplot as plt
 import random
 import glob
 import itertools
 import os
-
+import math
 import plotly.plotly as py
 import plotly.graph_objs as go
+import statistics as stat
 from plotly.tools import FigureFactory as FF
 
 import scipy
@@ -31,8 +34,8 @@ from matplotlib import pyplot
 # This code uses method one. I'd like to change it to method 2 if possible.
 
 ###Change myPath
-myPath = '/Users/DerekChang/Documents/Ragan Lab/Test/072919-NiFilmsBox6_D2-D6/D3'
-CombinedPath = '/Users/DerekChang/Documents/Ragan Lab/Test/072919-NiFilmsBox6_D2-D6/D3/Combined'
+myPath = '//Users/DerekChang/Documents/Ragan Lab/Characterization/08142019-Bx7 C1-C5/C1'
+CombinedPath = '/Users/DerekChang/Documents/Ragan Lab/Characterization/08142019-Bx7 C1-C5/C1/Combined'
 os.chdir(myPath)
 
 # SamplePath = '/Users/DerekChang/Documents/Ragan Lab/Test/072919-NiFilmsBox6_D2-D6/D3'
@@ -42,53 +45,60 @@ FileNames = glob.glob1(myPath, '*.txt') #list(?)
 numberofplots = len(FileNames)
 
 
+#[] array
+#{} dictionary
 ###Declared variables
-list = {}
-list2D = {}
-listG = {}
-ratio = {}
-a = {}
-b = {}
+list = []
+list2D = []
+listG = []
+ratio = []
+a = []
+b = []
+timmer = 0
 VisN = 0
 Number = 0
-Name2D = {}
+Name2D = []
 NameG = 0
 avgRatio = 0
+stdv = 0
 lengthR = 0
 sumR = 0
-wavenumber = {}
-wavenumber1 = {}
-
+sum1 = 0
+extra = 0
+N = 0
+wavenumber = []
+wavenumber1 = []
+ratio_list = []
 NewFile = []
-intensity = {}
-intensity1 = {}
-
-filenames = {}
-filenames2 = {}
-fileNamesTwoD = {}
-fileNamesG = {}
-gPeakArray = {}
-twoDPeakArray = {}
-# initialize G and 2D counter iterator for for loops
+intensity = []
+intensity1 = []
+filenames = []
+filenames2 = []
+fileNamesTwoD = []
+fileNamesG = []
+gPeakArray = []
+twoDPeakArray = []
 gCounter = 0
 twoDCounter = 0
 
 
-fig, axs = plt.subplots(numberofplots, 1, figsize=(10, 60), constrained_layout=False)
+fig, axs = plt.subplots(numberofplots, 1, figsize=(30, 25), constrained_layout=False)  ###nice figures
+# fig, axs = plt.subplots(numberofplots, 1, figsize=(5, 10), constrained_layout=False)  ###Computation
 fig.tight_layout()
-plt.rc('axes', labelsize='20')
-plt.rc('axes', titlesize='20')
+plt.rc('axes', labelsize='10')
+plt.rc('axes', titlesize='10')
+print('')
 
 for counter, datafile in enumerate(FileNames):  # enumerate splits a list up into a counter and its value (in this case, filenames.txt) into a 2 column list
     path = myPath + '/' + datafile
     # print(counter, datafile) #Should rename to filename later.
     #   print('counter is %i' % counter)
     # Save each filename in a list
-    filenames[counter] = datafile
+    filenames.append(datafile)
 
-    data_file = np.loadtxt(path, delimiter='\t', dtype="float", skiprows= 1)  # put skiprows=1 into loadtxt for new sets of data with headers still in.
-    wavenumber[counter] = data_file[:, 0]
-    intensity[counter] = data_file[:, 1]
+    data_file = np.loadtxt(path, delimiter='\t', dtype="float")  # put skiprows=1 into loadtxt for new sets of data with headers still in.
+    wavenumber.append(data_file[:, 0])
+    intensity.append(data_file[:, 1])
     # Strip txt from datafile and change to uppercase.
     strippedDatafile = datafile.rstrip('.txt')
     strippedDatafile = strippedDatafile.replace('-', '')
@@ -110,13 +120,13 @@ for counter, datafile in enumerate(FileNames):  # enumerate splits a list up int
     for iterator, peakpos in enumerate(x[indexes3]):
         # print(peakpos)
         if peakpos > 1540 and peakpos < 1600:
-            fileNamesG[gCounter] = np.array(strippedDatafile[:-1])
+            fileNamesG.append(np.array(strippedDatafile[:-1]))
             #   print('filename G is %s' % fileNamesG[counter])
             #listG = np.array([gCounter] = np.array(data_file)
-            listG[gCounter] = np.array(data_file)
+            listG.append(np.array(data_file))
             gpeak = y3[indexes3[iterator]]
-            gPeakArray[gCounter] = y3[indexes3[iterator]]
-            a[gCounter] = np.array(gPeakArray[gCounter])
+            gPeakArray.append(y3[indexes3[iterator]])
+            a.append(np.array(gPeakArray[gCounter]))
 
             gCounter = gCounter + 1
             #   print('gCounter is %i' % gCounter)
@@ -125,12 +135,12 @@ for counter, datafile in enumerate(FileNames):  # enumerate splits a list up int
             # print('G indexes are %f' % y3[indexes3[iterator]])
             # a = x[indexes3[iterator]]
         if peakpos > 2660 and peakpos < 2730:
-            fileNamesTwoD[twoDCounter] = np.array(strippedDatafile[:-2])
+            fileNamesTwoD.append(np.array(strippedDatafile[:-2]))
             # print('saved filename 2D is %s' % fileNamesTwoD[counter])
-            list2D[twoDCounter] = np.array(data_file)
+            list2D.append(np.array(data_file))
             twodpeak = y3[indexes3[iterator]]
-            twoDPeakArray[twoDCounter] = y3[indexes3[iterator]]
-            b[twoDCounter] = np.array(twoDPeakArray[twoDCounter])
+            twoDPeakArray.append(y3[indexes3[iterator]])
+            b.append(np.array(twoDPeakArray[twoDCounter]))
             twoDCounter = twoDCounter + 1
             # b = x[indexes3[iterator]]
             # print('2D indexes are %f' % y3[indexes3[iterator]])  # this is 2D intensity
@@ -162,11 +172,18 @@ for counter, datafile in enumerate(FileNames):  # enumerate splits a list up int
     #     plt.show()
     #     plt.savefig('Graphene-on-quartz.png')
 
+##delete extra plots
+for w in range(len(fileNamesTwoD)):
+    extra = len(fileNamesTwoD)+w
+    fig.delaxes(axs[extra])
 ### Matching
 for i in range(len(fileNamesTwoD)):
+    timmer = len(fileNamesTwoD) - i
+    print('Ready in %s' % timmer + "...")
     for j in range(len(fileNamesG)):
         if np.array_equal(fileNamesTwoD[i] ,fileNamesG[j]):
-            ratio[i] = np.array(b[i] / a[j], dtype="float")
+            # ratio[i] = np.array(b[i] / a[j], dtype="float")
+            ratio.append(np.array(b[i] / a[j], dtype="float"))
             def createFolder(directory):         ##creates folder if there isn't one exist already
                 try:
                     if not os.path.exists(directory):
@@ -175,7 +192,7 @@ for i in range(len(fileNamesTwoD)):
                     print('Error: Creating directory. ' + directory)
             createFolder('./Combined/')          ##folder name
 ## try to append the text files here listG[j] with list2D[i]
-            os.chdir(CombinedPath)
+            os.chdir(CombinedPath) 
             with open("2DG_Ratio_Combined.txt", "wb") as CR:
             # np.savetxt(listG[j].append(list2D[i]))
                 np.savetxt(CR, list2D[i], delimiter='\t')
@@ -184,28 +201,33 @@ for i in range(len(fileNamesTwoD)):
             # np.savetxt('2DG_Ratio_Combined.txt',listG[j], delimiter = '\t')
             #     VisN = len(fileNamesTwoD)-1
             # print('this is VisN', VisN)
-                NameG = fileNamesTwoD[j]
+                NameG = fileNamesTwoD[i]
                 os.rename('2DG_Ratio_Combined.txt', '%s' %NameG+".txt")
+                # print('Name:', NameG)
                 DataNames = glob.glob1(CombinedPath, '*.txt')
                 for counter2, dataNames in enumerate(DataNames):
                     os.chdir(CombinedPath)
                     Combined_Path = CombinedPath + '/' + dataNames
-                    filenames2[counter2] = dataNames
+                    filenames2.append(dataNames)
                     data_Names = np.loadtxt(Combined_Path, delimiter='\t', dtype="float")
-                    wavenumber1[counter2] = data_Names[:, 0]
-                    intensity1[counter2] = data_Names[:, 1]
+                    wavenumber1.append(data_Names[:, 0])
+                    intensity1.append(data_Names[:, 1])
                     axs[counter2].plot(wavenumber1[counter2], intensity1[counter2])
                     axs[counter2].set_xlabel(r'$Wavenumber\ (cm^{-1})$')
                     axs[counter2].set_ylabel('$Intensity\ (a.u.)$')
                     if len(DataNames) - 1 == counter2:
-                        # plt.show()
                         os.chdir(myPath)
                         plt.savefig('Graphene-on-quartz.png')
                 os.chdir(myPath)
-                if fileNamesTwoD[i] != fileNamesG[j]:
-                    j = j + 1
-                if fileNamesTwoD[i] == len(fileNamesG)-1:
-                    break
+            if fileNamesTwoD[i] != fileNamesG[j]:
+                j = j + 1
+                break
+        if fileNamesTwoD[i] == len(fileNamesG)-1:
+            break
+
+print('........done!')
+# plt.show()
+
 
 ###Graphing
 
@@ -228,13 +250,28 @@ for i in range(len(fileNamesTwoD)):
 
 # print('this is listg = ', listG)
 # print('file name',fileNamesTwoD)
+# print(ratio)
+# print(len(ratio))
+
+# for w in range(len(ratio)):    ##this calculates the average ratio
+#      sumR = sumR + ratio[w]
+#      if w > len(ratio):
+#          break\
+
+from array import *
 
 
-for w in range(len(ratio)):    ##this calculates the average ratio
-     sumR = sumR + ratio[w]
-     if w > len(ratio):
-         break
-avgRatio = sumR/len(ratio)
+# print('this is array', ratio)
+# print('this is array list', ratio_list)
+# print('this is list ', ratio_list)
+#this is wrong, counting the index but not data
+# print('this is ratio ', ratio)
+
+# avgRatio = sumR/len(ratio)
+ratio_list = np.array(ratio).tolist()
+avgRatio = stat.mean(ratio_list)
+stdv = stat.stdev(ratio_list)
+
 print('')
-print('this is average', avgRatio)
-# print('this is names ', NameG)
+print('Average = ', avgRatio)
+print('Standard Deviation = ', stdv)
